60de88ffbe66eb6d122f71904bcd997b43002591
charmidez <kodjoassiobo@protonmail.com>
2025-06-03 18:53:18 +0000

    ok

---
+++ src/app/components/NavBarMine.tsx
@@ -0,0 +1,8 @@
+export default function NavBarMine() {
+  return (
+    <div className="navbar shadow-sm bg-primary">
+      <h1 className="text-2xl font-bold">XITAT1ON</h1>
+      <a className="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+  );
+}
--- src/app/pages/HomeClients.tsx
+++ src/app/pages/HomeClients.tsx
@@ -1,8 +1,9 @@
+import NavBarMine from "../components/NavBarMine";
 
 export default function HomeClients() {
   return (
-    <div className="flex flex-col ">
-      <div> Citation à Télecharger</div>
+    <div className="flex flex-col lg:px-16 md:px-8 px-4 lg:py-8 md:py-4 py-2">
+        <NavBarMine />      
     </div>
   );
 }
